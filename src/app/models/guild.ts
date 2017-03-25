c8b8dbaff8ed9ec44f13adfce3c7041de05a861a
Justin Blau <justin.blau8@gmail.com>
2017-03-25 14:04:38 -0400

    added guild model

---
+++ src/app/models/guild.ts
@@ -0,0 +1,76 @@
+export class Guild{
+    guildId: string;
+    teams: Team[];
+    characters: Character[];
+    guildInventory: GuildInventory;
+}
+
+export class Character{
+    unitId: string;
+    baseCharacterId: string;
+    level: number;
+    stats: StatSheet;
+    equipmentSheet: string[];
+    name: string;
+    teamId: number;
+    teamSlotNumber: number;
+    regimen: string[];
+    skills: string[];
+}
+
+export class StatSheet{
+    id: number;
+    base: StatTypes;
+}
+
+export class StatTypes{
+    base: RawStats;
+    effective: RawStats;
+    current: RawStats;
+}
+
+export class RawStats{
+    strength: number;
+    agility: number;
+    dexterity: number;
+    intelligence: number;
+    vitality: number;
+    focus: number;
+}
+
+export class GuildInventory{
+    id: number;
+    equippableItems: Item;
+    nonEquippableItems: Item;
+    characterCards: CharacterCard[];
+    currency: number;
+    size: number;
+}
+
+export class Item{
+    name: string;
+    bonusSkills: string[];
+    bonusStats: StatTypes;
+    itemId: string;
+    itemTypeId: string;
+}
+
+export class Team{
+    currentZone: string;
+    teamId: number;
+    name: string;
+    units: string[];
+    primaryActionFinish: Date;
+    actionType: number;
+}
+
+export class CharacterCard{
+    cardId: string;
+    strength: number;
+    agility: number;
+    dexterity: number;
+    intelligence: number;
+    vitality: number;
+    focus: number;
+    name: string;
+}
