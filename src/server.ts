d9e4a05422a6d77ae51c24593858525f13dab415
Justin Blau <justin.blau8@gmail.com>
2017-05-12 18:33:26 -0400

    remove prod mode

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,6 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule);
 
 // Enable production mode unless running locally
-if (!/localhost/.test(document.location.host)) {
-  enableProdMode();
-}
+// if (!/localhost/.test(document.location.host)) {
+//   enableProdMode();
+// }
